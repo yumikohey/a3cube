b12cdb432aab87a0e8ec2bcdd253e27becbff4d4
yumikohey <dayumikoda@gmail.com>
2014-08-21 13:20:10 -0700

    pagination unable.

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $("#owl-demo").owlCarousel({
  
       autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
+      pagination: false,
       items : 4,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
@@ -15,7 +15,7 @@ $(document).ready(function() {
   });
  
     $("#soft-products").owlCarousel({
- 
+      pagination: false,
       items : 3,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
@@ -23,7 +23,7 @@ $(document).ready(function() {
   });
 
      $("#hard-products").owlCarousel({
- 
+      pagination: false,
       items : 3,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
